8054815258b502339a82f9b0e64f57207884073c
Nicolas Pinilla <spinillac28@gmail.com>
2023-03-28 19:02:04 -0500

    Metodos post, delete y put terminados

--- scripts/car.js
+++ scripts/car.js
@@ -1,4 +1,8 @@
 const tableBody = document.getElementById("tableBody")
+const id = document.getElementById("txtId")
+const marca = document.getElementById("txtMarca")
+const modelo = document.getElementById("txtModelo")
+const categoriaId = document.getElementById("txtCategoriaId")
 
 
 function obtenerCar(){
@@ -27,7 +31,82 @@ function obtenerCar(){
     .catch(error => alert(error));
 }
 
+function eliminarCar(){
+    var requestOptions = {
+        method: 'DELETE',
+        redirect: 'follow'
+      };
+      
+    fetch(`https://gd257f4deb7943f-ntvho1f0kb24nopq.adb.us-chicago-1.oraclecloudapps.com/ords/admin/car/car?id=${id.value}`, requestOptions)
+        .then(response => {
+            if(response.status == 204){
+                location.reload()
+            }else{
+                alert("Error al eliminar car")
+            }
+        })
+        .catch(error => console.log('error', error));
+}
+
+function crearCar(){
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "id": id.value,
+        "brand": marca.value,
+        "model": modelo.value,
+        "category_id": categoriaId.value
+    });
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch("https://gd257f4deb7943f-ntvho1f0kb24nopq.adb.us-chicago-1.oraclecloudapps.com/ords/admin/car/car", requestOptions)
+        .then(response => {
+            if(response.status == 201){
+                location.reload()
+            }else{
+                alert("Error al agregar car")
+            }
+        })
+        .catch(error => console.log('error', error));
+}
+
+function actualizarCar(){
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+    "id": id.value,
+    "brand": marca.value,
+    "model": modelo.value,
+    "category_id": categoriaId.value
+    });
+
+    var requestOptions = {
+    method: 'PUT',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch("https://gd257f4deb7943f-ntvho1f0kb24nopq.adb.us-chicago-1.oraclecloudapps.com/ords/admin/car/car", requestOptions)
+        .then(response => {
+            if(response.status == 201){
+                location.reload()
+            }else{
+                alert("Error al actualizar car")
+            }
+        })
+        .catch(error => console.log('error', error));
+}
+
 
 obtenerCar()
 
-/**/
+
